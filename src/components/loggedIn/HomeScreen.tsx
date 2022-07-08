8e95089184895e0d2cc6b6285e8540e01b3c514f
Filip Karlberg <filipkarlberg1@gmail.com>
2022-07-08 13:13:00 +0200

    🆕Add responsive grid

--- src/components/loggedIn/HomeScreen.tsx
+++ src/components/loggedIn/HomeScreen.tsx
@@ -11,13 +11,14 @@ export default function homeScreen( props : Props) {
     const { user } = props;
 
     return (
-        <main className='w-full h-screen flex flex-row'>
-            <Graph />
-            <div>
+        <main className='w-full h-screen flex flex-col justify-center items-center  md:flex-row'>
+            <div className='w-3/4 h-full center p-10'>
+                <Graph />
+            </div>
+            <div className='w-1/4 h-full center'>
                 <div>d</div>
                 <div>d</div>
             </div>
-            
         </main>
     )
 }
