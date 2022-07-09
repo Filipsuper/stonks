0a031aa1d750aff8069dd4a6beeb3c75f94bdbb9
Filip Karlberg <filipkarlberg1@gmail.com>
2022-07-09 18:55:10 +0200

    ✨ Backend init

---
+++ backend/index.ts
@@ -0,0 +1,3 @@
+import dataStreamer from "./websocket/websocket.js";
+
+setInterval(() => { console.log(dataStreamer("ETH-USD"))  }, 5000)
---
+++ backend/websocket/websocket.ts
@@ -0,0 +1,5 @@
+import WebSocket from "ws";
+
+export default function dataStreamer(ticker : string){
+    return "hello"
+}
